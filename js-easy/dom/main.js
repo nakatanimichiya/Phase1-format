6b2ea92b617a96a4639e1baf7f4c6da694120f0b
nakatanimichiya <mnakat01@alpha.shudo-u.ac.jp>
2021-04-19 12:07:37 +0900

    r

--- js-easy/dom/main.js
+++ js-easy/dom/main.js
@@ -19,11 +19,33 @@
 
 
 //アラート表示
-window.alert("これはwindow.alert()で表示したアラートです")
+// window.alert("これはwindow.alert()で表示したアラートです")
 
 //window.　に限り、省略することが出来る。
-alert("これはalert()で表示したアラートです")
+// alert("これはalert()で表示したアラートです")
 
 //documentのプロパティにアクセスする例
 console.log(document.doctype) //=> <!doctype html>
 console.log(document.title) //=> DOM API
+
+//<p id="intro">...</p>要素を取得
+//document.getElementById(ID)
+const intro = document.getElementById("intro")
+console.log(intro.textContent)  //これは紹介文です
+
+//<ul class="list">...</ul>要素を取得
+//document.querySelector(セレクタ)
+const list = document.querySelector(".list")
+console.log(list.children.length) // =>3
+
+//<ul class="list">..</ul>の中にある<li>要素を取得
+const items = document.querySelectorAll(".list li")
+
+//itemsは配列なので、forEachで繰り返しが出来る
+//forEachは、配列の要素を１つずつ取り出してループしたいとき使う
+// 配列.forEach((変数) => {
+//     console.log(変数.textContent)
+// })
+items.forEach((item) => {
+    console.log(item.textContent)
+})
