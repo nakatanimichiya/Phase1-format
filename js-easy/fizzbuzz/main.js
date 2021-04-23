2b3cb7a9f762e76282bc998c36617b4ccc1bf115
nakatanimichiya <mnakat01@alpha.shudo-u.ac.jp>
2021-04-23 11:08:31 +0900

    60

--- js-easy/fizzbuzz/main.js
+++ js-easy/fizzbuzz/main.js
@@ -1,21 +1,67 @@
+// let i = 0;
+
+// const rewriteCntUp = function () {
+//     const rewriteNum = document.getElementById("rewriteNum");
+//     
+
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         rewriteNum.innerHTML = "fizzbuzz";
+//     } else if (i % 3 === 0) {
+//         rewriteNum.innerHTML = "fizz";
+//     } else if (i % 5 === 0) {
+//         rewriteNum.innerHTML = "buzz";
+//     } else {
+//         rewriteNum.innerHTML = i;
+//     }
+//     i += 1;
+//
+// };
+
+// const title = document.getElementById("title")
+// console.log(`<h1>タグの中身のテキストは${title.textContent}です。`)
 let i = 0;
 
-const rewriteCntUp = function () {
-    const rewriteNum = document.getElementById("rewriteNum");
-    // for (let i = 1; i < 1000; i++) {
+const showNum = document.getElementById("showNum");
+showNum.innerHTML = i;
 
-    if (i % 3 === 0 && i % 5 === 0) {
-        rewriteNum.innerHTML = "fizzbuzz";
-    } else if (i % 3 === 0) {
-        rewriteNum.innerHTML = "fizz";
-    } else if (i % 5 === 0) {
-        rewriteNum.innerHTML = "buzz";
-    } else {
-        rewriteNum.innerHTML = i;
-    }
+const fizzBtn = function () {
     i += 1;
-    // }
+    if (i % 3 === 0) {
+        showNum.innerHTML = "fizz";
+    } else {
+        alert("miss");
+        location.reload();
+    }
 };
 
-const title = document.getElementById("title")
-console.log(`<h1>タグの中身のテキストは${title.textContent}です。`)
+const buzzBtn = function () {
+    i += 1;
+    if (i % 5 === 0) {
+        showNum.innerHTML = "buzz";
+    } else {
+        alert("miss");
+        location.reload();
+    }
+};
+
+const fizzbuzzBtn = function () {
+    i += 1;
+    if (i % 3 === 0 && i % 5 === 0) {
+        showNum.innerHTML = "fizzbuzz";
+    } else {
+        alert("miss");
+        location.reload();
+    }
+};
+
+const numBtn = function () {
+    i += 1;
+    if (i % 5 === 0 || i % 3 === 0) {
+        // showNum.innerHTML = "i";
+        // } else {
+        alert("miss");
+        location.reload();
+    }
+    showNum.innerHTML = i;
+};
+
