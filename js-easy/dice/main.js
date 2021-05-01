9ecf1692dbb64174da597faa0eccb98421fee385
nakatanimichiya <mnakat01@alpha.shudo-u.ac.jp>
2021-05-01 13:26:01 +0900

    firstCommit

--- js-easy/dice/main.js
+++ js-easy/dice/main.js
@@ -25,6 +25,7 @@
 const body = document.querySelector("body");
 const player1Btn = document.getElementById("player1Btn");
 const player2Btn = document.getElementById("player2Btn");
+const result = document.getElementById("result");
 
 const dice1 = document.getElementById("setPlayer1dice");
 dice1.style.width = "100px";
@@ -35,50 +36,77 @@ dice2.style.width = "100px";
 dice2.style.height = "100px";
 
 
-let diceNum = "./img/saikoro1.png"
+let diceNum1 = 1
+let diceNum2 = 1
+let player1Stop
+let player2Stop
+
+var timer1;
+var timer2;
+
 const random1 = function () {
-    dice1.setAttribute("src", diceNum)
-    diceNum = `./img/saikoro${Math.floor(Math.random() * 6 + 1)}.png`;
+    player1Btn.disabled = true
+    result.textContent = "???"
+    player1Stop = true
+    dice2.setAttribute("src", "./img/saikoro1.png")
+    diceNum1 = Math.floor(Math.random() * 6 + 1);
+    dice1.setAttribute("src", `./img/saikoro${diceNum1}.png`)
 }
 
-const random2 = function () {
-    dice2.setAttribute("src", diceNum)
-    diceNum = `./img/saikoro${Math.floor(Math.random() * 6 + 1)}.png`;
-}
 
-var timer;
 
 player1Btn.addEventListener("click", function () {
-    clearInterval(timer);
-    timer = setInterval("random1()", 100);
+    clearInterval(timer1);
+    timer1 = setInterval("random1()", 100);
     setTimeout(function () {
-        clearInterval(timer);
+        clearInterval(timer1);
+        player1Stop = false
+        if (player1Stop === false && player2Stop === false) {
+            if (diceNum1 > diceNum2) {
+                result.textContent = "player1の勝ち"
+            } else if (diceNum1 < diceNum2) {
+                result.textContent = "player2の勝ち"
+            } else {
+                result.textContent = "引き分け"
+            }
+            player1Btn.disabled = false
+            player2Btn.disabled = false
+            player1Stop = null
+            player2Stop = null
+        }
     }, 3000);
-    console.log(player1Btn)
 });
+
+const random2 = function () {
+    player2Btn.disabled = true
+    result.textContent = "???"
+    player2Stop = true
+    dice2.setAttribute("src", "./img/saikoro1.png")
+    diceNum2 = Math.floor(Math.random() * 6 + 1);
+    dice2.setAttribute("src", `./img/saikoro${diceNum2}.png`)
+}
 
 player2Btn.addEventListener("click", function () {
-    clearInterval(timer);
-    timer = setInterval("random2()", 100);
+    clearInterval(timer2);
+    timer2 = setInterval("random2()", 100);
     setTimeout(function () {
-        clearInterval(timer);
+        clearInterval(timer2);
+        player2Stop = false
+        if (player1Stop === false && player2Stop === false) {
+            if (diceNum1 > diceNum2) {
+                result.textContent = "player1の勝ち"
+            } else if (diceNum1 < diceNum2) {
+                result.textContent = "player2の勝ち"
+            } else {
+                result.textContent = "引き分け"
+            }
+            player1Btn.disabled = false
+            player2Btn.disabled = false
+            player1Stop = null
+            player2Stop = null
+        }
     }, 3000);
 });
-
-
-const result = document.getElementById("result");
-result = function () {
-
-    if () {
-        result.innerHTML("プレイヤー1の勝ち");
-    } else if () {
-        result.innerHTML("プレイヤー2の勝ち");
-    } else {
-        result.innerHTML("引き分け");
-    }
-};
-
-
 
 
 
